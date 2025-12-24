edc828ab7641bc4cf9a748da3245a8bf80fd73ca
Kun Ho KO <kunho.ko@microsoft.com>
2025-12-25 02:09:14 +0900

    Update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Header from './Header'
 import Content from './Content'
 import Footer from './Footer'
