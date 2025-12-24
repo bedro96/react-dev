b97c61dcd18ff6c043736ba4dbe5a8880c42d706
Kunho Ko <bedro96@naver.com>
2025-12-25 01:47:50 +0900

    Update files

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Chance } from 'chance';
+import { useState } from 'react';
 
-const chance = new Chance();
+    
+
 
 function Content() {
     const handleNameChange = () => {
@@ -9,14 +11,16 @@ function Content() {
     const int = Math.floor(Math.random() * names.length);
     return names[int];
     };
-     
+    const chance = new Chance();
+    const [ chanceName, setChanceName ] = useState(null);
+    
     return (
         <main>
             <p>This is the content section.</p>
             <p>Hello to: {handleNameChange()}</p>
-            <p>Random name from Chance: {chance.name()}</p>
+            <p>Random name from Chance: {(chanceName===null)? setChanceName(chance.name()) : chanceName}</p>
             <p>Random age from Chance: {chance.age()}</p>
-            
+
         </main>
     )
 }
