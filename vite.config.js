d4d4fc430ce39371328cb2d3f292bc33e5c6edda
Kun Ho KO <kunho.ko@microsoft.com>
2025-12-29 14:59:15 +0900

    Update files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+const postcssConfig = require('./postcss.config.cjs')
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  css: {
+    postcss: postcssConfig
+  }
+}))
